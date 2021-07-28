15587499a37ada3815c0acb804d57128b0f27bb4
Ben <lolitsaburrito@gmail.com>
2021-07-28 14:19:12 -0700

    Multiplayer fixes. Players can now refresh page and rejoin game

--- index.js
+++ index.js
@@ -22,41 +22,35 @@ io.on("connection", socket => {
 
 	let game;
 	// Check if this player is already in a game (i.e. they reloaded tab)
-	if (game = games.find(g => g.playerX.username === username || g.playerO && g.playerO.username === username)) {
+	if (game = games.find(g => g.playerX.username === username || (g.playerO && g.playerO.username === username))) {
 		// determine whether this is player X or O
 		const [player, otherPlayer] = game.playerX.username === username ? ['playerX', 'playerO'] : ['playerO', 'playerX'];
 		// Reassign socket id
 		game[player].socketId = socket.id;
-		// Join this new connection to the game.
-		socket.join(game.playerX.username);
 		// Send only this player a NEW_PLAYER_EVENT so they can update their frontend.
-		socket.emit(NEW_PLAYER_EVENT, { username: game[otherPlayer].username, isPlayerX: username === game.playerO.username });
-		return;
-	}
-
-	// Try to find an available slot in a game. If none exist, make a new game.
-	game = games.find(g => g.playerO === null);
-	if (!game) {
-		game = { playerX: { username, socketId: socket.id}, playerO: null, xIsNext: true, squares: Array(9).fill(null) };
-		games.push(game);
+		socket.emit(NEW_PLAYER_EVENT, { username: game[otherPlayer].username, isPlayerX: otherPlayer === 'playerX', squares: game.squares });
+	// Otherwise, this player is not in a game, so look for/create a game
 	} else {
-		game.playerO = { username, socketId: socket.id }
-		socket.emit(NEW_PLAYER_EVENT, { username: game.playerX.username, isPlayerX: true });
+		// Try to find an available slot in a game. If none exist, make a new game.
+		game = games.find(g => g.playerO === null);
+		if (!game) {
+			// If all games are full
+			game = { playerX: { username, socketId: socket.id }, playerO: null, xIsNext: true, squares: Array(9).fill(null) };
+			games.push(game);
+		} else {
+			// If a game with an empty slot exists
+			game.playerO = { username, socketId: socket.id }
+			socket.emit(NEW_PLAYER_EVENT, { username: game.playerX.username, isPlayerX: true, squares: game.squares });
+		}
 	}
-	
-	console.log("game:")
-	console.log(game)
-	// console.log("games length: " + games.length)
 
 	const gameId = game.playerX.username;
 	socket.join(gameId);
 
-	// console.log(`Sending a ${NEW_PLAYER_EVENT}`)
-	socket.in(gameId).emit(NEW_PLAYER_EVENT, {username, isPlayerX: username === game.playerX.username});
-
 	// Listen for new messages
 	socket.on(NEW_GAME_MOVE_EVENT, ({ square, senderId }) => {
 		if (senderId === game[game.xIsNext ? 'playerX' : 'playerO'].socketId && game.squares[square] === null) {
+			game.squares[square] = game.xIsNext ? 'X' : 'O'
 			game.xIsNext = !game.xIsNext;
 			socket.in(gameId).emit(NEW_GAME_MOVE_EVENT, square);
 		} else {
